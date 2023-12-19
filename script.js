fa5ade4612251a8912688b8748f4e379ea09de8c
MeepMe3p <elijahsabay@gmail.com>
2023-12-20 05:33:22 +0800

    gwapoko

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-function adjustRatio(){
-    var slider = document.getElementById('slider');
-    var imgs = slider.getElementsByTagName('img');
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+signinBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+    title.innerHTML= "Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
 
-    if(imgs.length > 0){
-        var aspectRatio = imgs[0].naturalWidth/imgs[0].naturalHeight;
-        slider.style.aspectRatio;
-    }
 }
+
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = "60px";
+    title.innerHTML= "Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    function login() {
+        // Get the values of emailField and passwordField
+        var emailField = document.getElementById("emailField").querySelector("input").value;
+        var passwordField = document.getElementById("passwordField").querySelector("input").value;
+
+        if (emailField === "elijah" && passwordField === "12345") {
+            // Open the profile.html file or perform the desired action
+            window.location.href = "profile.html";
+        } else {
+            // Display an error message or perform other actions for unsuccessful login
+            console.log("Login failed. Incorrect credentials.");
+        }
+    }
+});
+
